cd73219f4793b9c16494a5b4d18748b967be055c
Pedro Guedes <pedrogudes58@hotmail.com>
2023-08-29 18:42:49 -0300

    Fist commit

---
+++ Socorro/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       try {
+           Class.forName("org.postgresql.Driver");
+           System.out.println("Driver carregado com sucesso");
+
+       }catch (Exception e){
+           System.out.println("Erro");
+
+       }
+    }
+}
